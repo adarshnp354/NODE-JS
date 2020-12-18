2e3bfc91eb6dbfcf545f42d3b353237eec49c098
adarshnp354 <74529927+adarshnp354@users.noreply.github.com>
2020-12-18 15:39:27 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const app=express();
+
+
+app.listen(3000,function(){
+    console.log("listening 3000")
+})
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname +'/index.html')
+})
+
+app.post('/quotes',(req,res)=>{
+    console.log(req.body)
+})
